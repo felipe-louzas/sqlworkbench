e17d2ecf9388a0aa06e8661021e98e1ab3c5fedb
Thomas Kellerer <support@sql-workbench.eu>
2020-01-04 21:47:16 +0000

    Update copyright

--- src/workbench/sql/wbcommands/WbListDependencies.java
+++ src/workbench/sql/wbcommands/WbListDependencies.java
@@ -2,7 +2,7 @@
  *
  * This file is part of SQL Workbench/J, https://www.sql-workbench.eu
  *
- * Copyright 2002-2019, Thomas Kellerer
+ * Copyright 2002-2020, Thomas Kellerer
  *
  * Licensed under a modified Apache License, Version 2.0
  * that restricts the use for certain governments.
