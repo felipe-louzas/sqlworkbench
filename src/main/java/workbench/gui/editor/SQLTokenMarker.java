3a7d2cd5a47c52e886228ee7fb2d4beb75dca732
Thomas Kellerer <codeberg@sql-workbench.eu>
2022-06-22 20:29:12 +0200

    Improve syntax highlighting with missing whitespace

--- src/main/java/workbench/gui/editor/SQLTokenMarker.java
+++ src/main/java/workbench/gui/editor/SQLTokenMarker.java
@@ -137,6 +137,8 @@ public class SQLTokenMarker
           }
           break;
         case ' ':
+        case '+':
+        case '=':
         case '\t':
         case ';':
           if (token == Token.NULL)
