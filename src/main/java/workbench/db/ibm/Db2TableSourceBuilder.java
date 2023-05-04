d95bdd38fbf943a3d2fc3c53a42f9b8eff8f38a6
Thomas Kellerer <codeberg@sql-workbench.eu>
2023-05-04 22:18:13 +0200

    Improve DDL generation for tables for DB2/LUW

--- src/main/java/workbench/db/ibm/Db2TableSourceBuilder.java
+++ src/main/java/workbench/db/ibm/Db2TableSourceBuilder.java
@@ -37,6 +37,8 @@ import workbench.db.TableIdentifier;
 import workbench.db.TableSourceBuilder;
 import workbench.db.WbConnection;
 
+import workbench.util.StringUtil;
+
 /**
  *
  * @author Thomas Kellerer
@@ -44,7 +46,8 @@ import workbench.db.WbConnection;
 public class Db2TableSourceBuilder
   extends TableSourceBuilder
 {
-  private boolean checkHistoryTable;
+  private final boolean checkHistoryTable;
+  private final boolean readTablespaceInfo;
   private boolean useSystemProc = false;
   private final DBID dbid;
 
@@ -53,15 +56,24 @@ public class Db2TableSourceBuilder
     super(con);
     dbid = DBID.fromConnection(dbConnection);
     checkHistoryTable = (dbid == DBID.DB2_LUW && JdbcUtils.hasMinimumServerVersion(con, "10.1"));
+    readTablespaceInfo = (dbid == DBID.DB2_LUW && con.getDbSettings().getBoolProperty("table_options.retrieve", true));
     useSystemProc = Db2GenerateSQL.useGenerateSQLProc(dbConnection, Db2GenerateSQL.TYPE_TABLE);
   }
 
   @Override
   public void readTableOptions(TableIdentifier table, List<ColumnIdentifier> columns)
   {
-    if (!checkHistoryTable) return;
     if (table == null) return;
     if (table.getSourceOptions().isInitialized()) return;
+
+    readPeriods(table);
+    readTablespaceInfo(table);
+    table.getSourceOptions().setInitialized();
+  }
+
+  private void readPeriods(TableIdentifier table)
+  {
+    if (!checkHistoryTable) return;
 
     String sql =
       "select periodname, \n" +
@@ -78,8 +90,7 @@ public class Db2TableSourceBuilder
     String tablename = table.getTableName();
     String schema = table.getSchema();
 
-
-    LogMgr.logMetadataSql(new CallerInfo(){}, "table options", sql, schema, tablename);
+    LogMgr.logMetadataSql(new CallerInfo(){}, "period definitions", sql, schema, tablename);
 
     try
     {
@@ -110,13 +121,136 @@ public class Db2TableSourceBuilder
     }
     catch (Exception e)
     {
+      LogMgr.logMetadataError(new CallerInfo(){}, e, "period definitions", sql, schema, tablename);
+    }
+    finally
+    {
+      JdbcUtils.closeAll(rs, stmt);
+    }
+  }
+
+  private void readTablespaceInfo(TableIdentifier table)
+  {
+    if (!readTablespaceInfo) return;
+
+    String sql =
+      "select tbspace, index_tbspace, long_tbspace, \n" +
+      "       property, \n" +
+      "       compression, rowcompmode, \n" +
+      "       datacapture, \n" +
+      "       encoding_scheme, \n" +
+      "       droprule \n" +
+      "from syscat.tables \n" +
+      "where tabschema = ? \n" +
+      "  and tabname = ? ";
+    PreparedStatement stmt = null;
+    ResultSet rs = null;
+
+    String tablename = table.getTableName();
+    String schema = table.getSchema();
+
+    LogMgr.logMetadataSql(new CallerInfo(){}, "table options", sql, schema, tablename);
+
+    try
+    {
+      stmt = this.dbConnection.getSqlConnection().prepareStatement(sql);
+      stmt.setString(1, schema);
+      stmt.setString(2, tablename);
+      rs = stmt.executeQuery();
+
+      if (rs.next())
+      {
+        String tbSpace = rs.getString("TBSPACE");
+        String idxSpace = StringUtil.trimToNull(rs.getString("INDEX_TBSPACE"));
+        String longSpace = StringUtil.trimToNull(rs.getString("LONG_TBSPACE"));
+        String props = rs.getString("PROPERTY");
+        String compression = rs.getString("COMPRESSION");
+        String rowMode = rs.getString("ROWCOMPMODE");
+        String capture = rs.getString("DATACAPTURE");
+        String encoding = rs.getString("ENCODING_SCHEME");
+        String dropRule = rs.getString("DROPRULE");
+
+        ObjectSourceOptions options = table.getSourceOptions();
+        options.addConfigSetting("tablespace", tbSpace);
+        String addSql = "IN " + dbConnection.getMetadata().quoteObjectname(tbSpace);
+
+        if (idxSpace != null)
+        {
+          options.addConfigSetting("index_tablespace", idxSpace);
+          addSql += " INDEX IN " + dbConnection.getMetadata().quoteObjectname(idxSpace);
+        }
+        if (longSpace != null)
+        {
+          options.addConfigSetting("long_tablespace", longSpace);
+          addSql += " LONG IN " + dbConnection.getMetadata().quoteObjectname(idxSpace);
+        }
+
+        if (props != null && props.length() > 19)
+        {
+          String columnTable = props.substring(19,20);
+          if ("Y".equals(columnTable))
+          {
+            options.addConfigSetting("organize", "column");
+            addSql += "\nORGANIZE BY COLUMN";
+          }
+        }
+
+        String compressDef = "";
+
+        if ("R".equals(compression) || "B".equals(compression))
+        {
+          compressDef = "COMPRESS YES";
+          if ("A".equals(rowMode))
+          {
+            options.addConfigSetting("compress", "adaptive");
+            compressDef += " ADAPTIVE";
+          }
+          else if ("S".equals(rowMode))
+          {
+            options.addConfigSetting("compress", "static");
+            compressDef += " STATIC";
+          }
+          if ("B".equals(compression))
+          {
+            compressDef += " VALUE COMPRESSION";
+            options.addConfigSetting("value_compression", "y");
+          }
+          addSql += "\n" + compressDef;
+        }
+
+        if ("Y".equals(capture))
+        {
+          options.addConfigSetting("datacapture", "y");
+          addSql += "\nDATA CAPTURE CHANGES";
+        }
+
+        if ("A".equals(encoding))
+        {
+          options.addConfigSetting("ccsid", "ascii");
+          addSql += "\nCCSID ASCII";
+        }
+        if ("U".equals(encoding))
+        {
+          options.addConfigSetting("ccsid", "unicode");
+          addSql += "\nCCSID UNICODE";
+        }
+
+        if ("R".equals(dropRule))
+        {
+          options.addConfigSetting("droprule", "restrict");
+          addSql += "\nWITH RESTRICT ON DROP";
+        }
+        table.getSourceOptions().setTableOption(addSql);
+      }
+    }
+    catch (Exception e)
+    {
       LogMgr.logMetadataError(new CallerInfo(){}, e, "table options", sql, schema, tablename);
     }
     finally
     {
       JdbcUtils.closeAll(rs, stmt);
     }
-    table.getSourceOptions().setInitialized();
   }
 
   @Override
